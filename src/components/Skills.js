ea22e47bb4ba88e679c339382d361716ef9c9f7a
hunghvu <hunghvu2017@gmail.com>
2021-06-28 18:55:17 -0700

    Fill in skills table

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,7 @@ import JavaScriptLogo from '../assets/skills/languages/1-js-logo.svg';
 import HtmlLogo from '../assets/skills/languages/2-html-logo.svg';
 import CssLogo from '../assets/skills/languages/3-css-logo.svg';
 import JavaLogo from '../assets/skills/languages/4-java-logo.svg';
-import Python from '../assets/skills/languages/5-python-logo.svg';
+import PythonLogo from '../assets/skills/languages/5-python-logo.svg';
 import CLogo from '../assets/skills/languages/6-c-logo.svg';
 import PostgreSqlLogo from '../assets/skills/languages/7-postgresql-logo.svg';
 
@@ -24,12 +24,6 @@ import AndroidLogo from '../assets/skills/os/3-android-logo.svg';
 import GitLogo from '../assets/skills/other/1-git-logo.svg';
 import PostmanLogo from '../assets/skills/other/2-postman-logo.svg';
 
-
-
-
-
-
-
 const SkillsComponent = () => {
 
   return (
@@ -40,55 +34,107 @@ const SkillsComponent = () => {
         </div>
       </div>
       <Container>
+
         <Row>
           <Col>
-            <em>Languages</em>
+            <h4>Languages</h4>
           </Col>
           <Col>
-            <em>Framework/Libraries</em>
+            <h4>Framework/Libraries</h4>
           </Col>
           <Col>
-            <em>Operating Systems</em>
+            <h4>Operating Systems</h4>
           </Col>
           <Col>
-            <em>Other Technologies</em>
+            <h4>Other Technologies</h4>
           </Col>
         </Row>
+
         <Row>
           <Col>
             <img src={JavaScriptLogo} alt="JavaScript Logo" />
           </Col>
           <Col>
-            <img src={ReactLogo} alt="React and ReactNative Logo" />
+            <img src={ReactLogo} alt="React and React Native Logo" />
           </Col>
-
+          <Col>
+            <img src={WindowsLogo} alt="WindowsLogo" />
+          </Col>
+          <Col>
+            <img src={GitLogo} alt="Git version control system Logo" />
+          </Col>
         </Row>
+
         <Row>
           <Col>
             <img src={HtmlLogo} alt="HTML5 Logo" />
           </Col>
-
+          <Col>
+            <img src={BootstrapLogo} alt="Bootstrap Logo" />
+          </Col>
+          <Col>
+            <img src={LinuxLogo} alt="Linux Logo" />
+          </Col>
+          <Col>
+            <img src={PostmanLogo} alt="Postman Logo" />
+          </Col>
         </Row>
+
         <Row>
           <Col>
             <img src={CssLogo} alt="CSS3 Logo" />
           </Col>
-
+          <Col>
+            <img src={JqueryLogo} alt="JQuery Logo" />
+          </Col>
+          <Col>
+            <img src={AndroidLogo} alt="Android Logo" />
+          </Col>
+          <Col xs="3"></Col>
         </Row>
+
         <Row>
           <Col>
             <img src={JavaLogo} alt="Java Logo" />
           </Col>
-
         </Row>
+
         <Row>
           <Col>
-            <img src={HtmlLogo} alt="HTML5 Logo" />
+            <img src={PostgreSqlLogo} alt="PostgreSQL Logo" />
           </Col>
-
         </Row>
 
+        <Row>
+          <Col className="text-center">
+            <em>With some experience in</em>
+          </Col>
+        </Row>
 
+        <Row>
+          <Col xs="3">
+            <img src={PythonLogo} alt="Python Logo" />
+          </Col>
+          <Col>
+            <img src={NodeJsLogo} alt="NodeJS Logo" />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs="3">
+            <img src={CLogo} alt="C Logo" />
+          </Col>
+          <Col>
+            <img src={ExpressLogo} alt="Express Logo" />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs="3"></Col>
+          <Col>
+            <img src={IonicLogo} alt="Ionic Logo" />
+          </Col>
+        </Row>
       </Container>
     </div>
   );
